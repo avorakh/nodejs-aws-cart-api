b57add50074ff660c7e680c85a31a2cdc18cac11
Aliaksandr Vorakh <aliaksandrvorakh@gmail.com>
2024-07-16 21:59:27 +0200

    wrap Nest.js application to AWS Lambda

--- src/main.ts
+++ src/main.ts
@@ -1,21 +1,22 @@
 import { NestFactory } from '@nestjs/core';
-
-import helmet from 'helmet';
-
 import { AppModule } from './app.module';
+import { Handler, Context, Callback } from 'aws-lambda';
+import { ExpressAdapter } from '@nestjs/platform-express';
+import * as express from 'express';
+import * as awsServerlessExpress from 'aws-serverless-express';
+import { Server } from 'http';
 
-const port = process.env.PORT || 4000;
+let server: Server;
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-
-  app.enableCors({
-    origin: (req, callback) => callback(null, true),
-  });
-  app.use(helmet());
-
-  await app.listen(port);
+  const expressApp = express();
+  const adapter = new ExpressAdapter(expressApp);
+  const app = await NestFactory.create(AppModule, adapter);
+  await app.init();
+  return awsServerlessExpress.createServer(expressApp);
 }
-bootstrap().then(() => {
-  console.log('App is running on %s port', port);
-});
+
+export const handler: Handler = async (event: any, context: Context, callback: Callback) => {
+  server = server ?? (await bootstrap());
+  return awsServerlessExpress.proxy(server, event, context, 'PROMISE').promise;
+};
