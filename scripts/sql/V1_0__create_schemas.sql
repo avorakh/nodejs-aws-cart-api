26ce4dcf3df3a8e747000faad38ba6a36cf8e0fa
Aliaksandr Vorakh <aliaksandrvorakh@gmail.com>
2024-07-18 22:16:12 +0200

    Add SQL script to fill tables

--- scripts/sql/V1_0__create_schemas.sql
+++ scripts/sql/V1_0__create_schemas.sql
@@ -9,8 +9,8 @@ END $$;
 CREATE TABLE IF NOT EXISTS carts (
     id UUID PRIMARY KEY,
     user_id UUID NOT NULL,
-    created_at DATE NOT NULL,
-    updated_at DATE NOT NULL,
+    created_at DATE NOT NULL DEFAULT current_date,
+    updated_at DATE NOT NULL DEFAULT current_date,
     status cart_status NOT NULL
 );
 
@@ -33,3 +33,4 @@ CREATE TABLE IF NOT EXISTS orders (
     status TEXT,
     total NUMERIC
 );
+
