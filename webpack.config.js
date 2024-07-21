f27d7c85db0fbd40e1084d83ad99638bd05dfc15
Aliaksandr Vorakh <aliaksandrvorakh@gmail.com>
2024-07-21 15:26:12 +0200

    fix webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,28 @@
-module.exports = function (options, webpack) {
-  return {
-    ...options,
-    entry: ['./src/lambda.ts'],
-    externals: [],
-    output: {
-      ...options.output,
-      libraryTarget: 'commonjs2',
-    },
-    plugins: [
-      ...options.plugins,
-      }),
+const path = require('path');
+const nodeExternals = require('webpack-node-externals');
+const { RunScriptWebpackPlugin } = require('run-script-webpack-plugin');
+
+module.exports = {
+  entry: ['./src/lambda.ts'],
+  target: 'node',
+  externals: [nodeExternals()],
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
     ],
-  };
+  },
+  resolve: {
+    extensions: ['.ts', '.js'],
+  },
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: 'main.js',
+  },
+  plugins: [
+    new RunScriptWebpackPlugin({ name: 'main.js' }),
+  ],
 };
