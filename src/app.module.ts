c3c3aa0ee61bf86150392bedc246f6f56bf953af
Aliaksandr Vorakh <aliaksandrvorakh@gmail.com>
2024-07-30 21:09:24 +0200

    Integrate TypeORM

--- src/app.module.ts
+++ src/app.module.ts
@@ -10,6 +10,19 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
   imports: [
+  TypeOrmModule.forRoot({
+    type: 'postgres',
+    host: process.env.DB_HOST,
+    port: parseInt(process.env.DB_PORT, 10),
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    entities: [Cart],
+    synchronize: false,
+    ssl: {
+      rejectUnauthorized: false
+    }
+  }),
     AuthModule,
     CartModule,
     OrderModule,
